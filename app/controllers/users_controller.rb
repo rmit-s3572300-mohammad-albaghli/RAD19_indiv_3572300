bc98c4f132cd3271bdcef13d5f969ef6ab1d76a5
Mohammad Albaghli <s3572300@student.rmit.edu.au>
2019-05-02 10:41:45 +0000

    New footer, QoL updates elsewhere

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,42 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])  
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "You've successfully signed up!"
+      log_in @user
       redirect_to :controller => 'static_pages', :action => 'home'
     else
       render 'new'
     end
   end
-
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
-
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
