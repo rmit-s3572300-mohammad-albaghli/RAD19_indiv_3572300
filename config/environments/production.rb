7da550a34770ddd94533afe7e12d3b7802d8e05c
Mohammad Albaghli <s3572300@student.rmit.edu.au>
2019-05-28 07:43:03 +0000

    Add email stuff to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "assignment_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'yourcourse.3572300@gmail.com',
+    password:             'Kake1234!',
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
