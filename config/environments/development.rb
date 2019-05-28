f5b08d9fc2228218f63fc75b061273ea82d92a35
Mohammad Albaghli <s3572300@student.rmit.edu.au>
2019-05-28 08:00:46 +0000

    fix typo in production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.exception_app = self.routes
+  config.exceptions_app = self.routes
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
